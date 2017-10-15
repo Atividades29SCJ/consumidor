1992a5701be53389ee2582aba05873efe4a6e57e
guilhermeandrade <guilhermeandrade.ti@gmail.com>
2017-10-14 23:26:41 -0300

    first commit

---
+++ src/main/java/br/com/fiap/consumidor/load/Loader.java
@@ -0,0 +1,18 @@
+package br.com.fiap.consumidor.load;
+
+import javax.xml.ws.Endpoint;
+
+import br.com.fiap.consumidor.service.ConsumidorService;
+
+public class Loader {
+
+	public static void main(String[] args) {
+		
+		String port = System.getenv("PORT"); 
+//		String port = "8080";
+        String host = "http://0.0.0.0:";
+        String uri = "/service";
+        ConsumidorService service = new ConsumidorService();
+        Endpoint.publish(host + port + uri, service);
+	}
+}
